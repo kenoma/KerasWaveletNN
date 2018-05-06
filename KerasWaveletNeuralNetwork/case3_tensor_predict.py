4b9708cf755ec29e7a4cd4d0fb6dc5646d567764
kenoma <bghati@yandex.ru>
2018-05-06 20:04:44 +0300

    эксперименты

--- KerasWaveletNeuralNetwork/case3_tensor_predict.py
+++ KerasWaveletNeuralNetwork/case3_tensor_predict.py
@@ -14,69 +14,51 @@ from matplotlib.patches import Ellipse
 
 Amplitude_line1, Marking_line1, Amplitude_line2, Marking_line2, Amplitude_line3, Marking_line3, Amplitude_line4, Marking_line4 = np.genfromtxt("train_5820_3333.csv", delimiter = ';', unpack = True,skip_header=1)
 
-x = Amplitude_line1 - sum(Amplitude_line1) / len(Amplitude_line1)
+x = Amplitude_line1-sum(Amplitude_line1)/len(Amplitude_line1)
 x = x / max(x)
-
-y = Marking_line1
 X = []
 Y = []
-slice = 1
-for i in range(slice, len(x)):
-    X.append([a for a in x[(i - slice) : i]])
-    tmpy = np.zeros(3)
-    tmpy[int(round(y[i]))] = 1
-    Y.append(tmpy)
+slice = 10
+for i in range(slice, len(x)-1):
+    X.append([a for a in x[(i - slice):i]])
+    Y.append(x[i+1])
 
 print("Total samples:",len(X))
 
-#x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.1)
-x_train = X[1:len(X) - 10000]
-y_train = Y[1:len(X) - 10000]
-x_test = X[len(X) - 10000:len(X)]
-y_test = Y[len(X) - 10000:len(X)]
+x_train = X[1:len(X)-10000]
+y_train = Y[1:len(X)-10000]
+x_test = X[len(X)-10000:len(X)]
+y_test = Y[len(X)-10000:len(X)]
 
-wavelons = 1000
-
-f_layer = WNN(wavelons, 3,
-                     input_dim = slice)
+wavelons = 100
+f_layer = WNN(wavelons, 1, input_dim = slice)
 
 model = Sequential()
 model.add(f_layer)
-model.add(Dense(15, activation='softmax'))
-model.add(Dense(3, activation='softmax'))
-
 
 model.compile(loss='mean_squared_error',
               optimizer='adagrad',
-              metrics=['categorical_crossentropy'])
+              metrics=['mae'])
 
 model.fit(np.array(x_train), 
           np.array(y_train),
-          epochs = 1000,
+          epochs = 200,
           verbose = 1,
-          batch_size = 1000)
+          batch_size = 100)
 
 score = model.evaluate(np.array(x_test), np.array(y_test), verbose=True) 
 print(score)
 
-pred = model.predict_classes(np.array(x_test))
-px = []
-py = []
-for i in range(0,len(y_test)):
-    if(pred[i] != 0):
-        px.append(i)
-        py.append(x_test[i][-1])
-
-print(px)
+pred = model.predict(np.array(x_test))
 
 plt.ion()
 plt.show()
 plt.clf()
-plt.title('tensor sig_1')
+plt.title('tensor sig 1')
 plt.ylabel('n')
 plt.xlabel('y')
 
-plt.plot([a[-1] for a in x_test], label='test', c=(0,0,0))
-plt.scatter(px, py, c=(1,0,0), alpha=0.5, s=1)
+plt.plot(y_test, label='test', c=(0,0,0))
+plt.plot(pred, label='pred', c=(1,0,0))
 plt.show()
 plt.pause(120)
